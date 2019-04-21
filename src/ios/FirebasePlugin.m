6d3478dde91a2b7f2e0d813f11faae2cf8010416
Ram Mehta <ram.mehta@gmail.com>
2019-04-21 15:14:14 -0400

    Add support for getting iOS APNS token.

--- src/ios/FirebasePlugin.m
+++ src/ios/FirebasePlugin.m
@@ -2,6 +2,7 @@
 #import <Cordova/CDV.h>
 #import "AppDelegate.h"
 #import "Firebase.h"
+#import "NSData+Serialize.h"
 #import <Fabric/Fabric.h>
 #import <Crashlytics/Crashlytics.h>
 @import FirebaseInstanceID;
@@ -62,6 +63,12 @@ static FirebasePlugin *firebasePlugin;
 
 - (void)getToken:(CDVInvokedUrlCommand *)command {
     CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[[FIRInstanceID instanceID] token]];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)getAPNSToken:(CDVInvokedUrlCommand *)command {
+    NSData *rawToken = [FIRMessaging messaging].APNSToken;
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[rawToken serializeToHexString]];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
